5fa3f10496927c0674e9b82f637d508998d418e3
Bhuvana <bhuvanamalla@gmail.com>
2024-04-15 18:52:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,34 @@
 import streamlit as st
 import pickle
 import string
-import nltk
 from nltk.corpus import stopwords
 
 from nltk.stem.porter import PorterStemmer
-
+import nltk
+nltk.download('punkt')
 ps = PorterStemmer()
-
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
-
     y = []
     for i in text:
         if i.isalnum():
             y.append(i)
-
     text = y[:]
     y.clear()
-
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
-
     text = y[:]
     y.clear()
-
     for i in text:
         y.append(ps.stem(i))
-
     return " ".join(y)
-
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
-
 st.title("SMS Spam Predictor")
-
-input_sms = st.text_area("Enter your message below :")
-
-if st.button('Let\'s check'):
-
+input_sms = st.text_area("Enter your msg below :")
+if st.button('Lets check'):
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
     # 2. vectorize
